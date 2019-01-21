422583fe6f3b8c4ed8bb18c9cec1998a3ef5a5fc
Bas Vodde <basv@odd-e.com>
2019-01-21 17:42:15 +0800

    display Welcome message

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import App from '../App';
 import Welcome from '../Welcome';
 import '../setupTests';
 
 describe('Welcome Page', () => {
   it('Welcome component has the right text',() => {
-    const welcome = mount(<Welcome/>);
-    expect(welcome.text()).toEqual('Welcome Quote Seeker');
+    const app = mount(<App/>);
+    expect(app.find('Welcome').length).toBe(1);
+    expect(app.find('Welcome').text()).toEqual('Welcome Quote Seeker');
   });
 });
