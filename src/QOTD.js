fcb886bee95f5f76042e4dad620b5bfc36b63cc8
Terry Yin <terry.yinzhe@gmail.com>
2019-01-23 11:49:12 +0800

    call graphql

--- src/QOTD.js
+++ src/QOTD.js
@@ -1,4 +1,29 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_QUOTE_OF_TODAY = gql`
+  {
+    getQuoteOfTheDay {
+        content
+         originalAuthor
+         submitter
+    }
+  }
+`;
+
+const QuoteOfTheDay = () => (
+  <Query query={GET_QUOTE_OF_TODAY}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+      <div>{data.getQuoteOfTheDay.content}</div>
+      );
+    }}
+  </Query>
+);
 
 export default class QOTD extends Component {
 
@@ -16,7 +41,9 @@ export default class QOTD extends Component {
 
     render() {
         return (
-            <div>{this.quote}</div>
+            <div>
+            <QuoteOfTheDay />
+            {this.quote}</div>
         );
     }
 }
