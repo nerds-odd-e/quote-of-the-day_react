e9e7d9ad4d8854b399a59c13caa069ec8da47b04
Terry Yin <terry.yinzhe@gmail.com>
2019-01-24 09:40:30 +0800

    fix app test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../App';
 import QuoteOfTheDay from '../QuoteOfTheDay';
 
 import '../setupTests';
 
 describe('Welcome Page', () => {
-  xit('Welcome component has the right text', () => {
-    const app = mount(<App />);
-    expect(app.find('Welcome').text()).toEqual('Welcome Quote Seeker');
-  });
-
-  xit('should render QOTD component', () => {
-    let app = mount(<App />);
+  it('should render QOTD component', () => {
+    let app = shallow(<App />);
     expect(app.containsMatchingElement(<QuoteOfTheDay />)).toEqual(true);
   });
 
